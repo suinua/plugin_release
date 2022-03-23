a1399cffbba62fb662922178b9639c8e1a93bbfb
suinua <34801662+suinua@users.noreply.github.com>
2022-03-23 23:09:04 +0900

    test

--- bin/plugin_release.dart
+++ bin/plugin_release.dart
@@ -5,5 +5,5 @@ void main() {
   var env = Platform.environment;
   final pluginDirPath = path.join(env['GITHUB_WORKSPACE']!,env['PLUGIN_DIR']);
   final pluginYamlPath = path.join(pluginDirPath, 'plugin.yml');
-  print(env['GITHUB_EVENT_PATH']);
+  print(File(env['GITHUB_EVENT_PATH']!).readAsStringSync());
 }
